634fca9e6fa435d00bdc729260a2495270165fdb
sieugene <gagarin126rus@Mail.ru>
2020-06-04 15:18:02 +0300

    Updates

---
+++ precache-manifest.ae4372fa4e6517b2fd3f2af671ac4b27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e516818b915e6ca1814aed0865b93f98",
+    "url": "/bim/index.html"
+  },
+  {
+    "revision": "9f09bb2984803bea2157",
+    "url": "/bim/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "d8b2f3736c521c58e22a",
+    "url": "/bim/static/css/main.c5f0ed60.chunk.css"
+  },
+  {
+    "revision": "9f09bb2984803bea2157",
+    "url": "/bim/static/js/2.a4fa2393.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/bim/static/js/2.a4fa2393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b2f3736c521c58e22a",
+    "url": "/bim/static/js/main.d0a791fb.chunk.js"
+  },
+  {
+    "revision": "3a5abb868d41970c3994",
+    "url": "/bim/static/js/runtime-main.eee68c44.js"
+  }
+]);
